c2e433c5d60cd633154d4246f64d1639bbb601cf
Alumno <Alumno@PC30>
2019-08-20 09:52:20 -0500

    2.0

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Imprime los numeros del x hasta Y de -1 ");
+            Console.WriteLine("De donde arrancamos el ciclo  (mayor): ");
+
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De donde terminamos el ciclo: (menor): ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            for (int i = x; i > y; i -= z)
+            {
+                Console.WriteLine("i = " + i);
+            }
+
+        }
+    }
+}
